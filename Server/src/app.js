a78189a1cf94112ee8bc509e6b3272a5b2ff71f6
Juan Carlos Valero Vergara <123209565+JCValeroVergara@users.noreply.github.com>
2023-11-01 23:30:09 -0500

    Update app.js

--- Server/src/app.js
+++ Server/src/app.js
@@ -5,17 +5,14 @@ const routerApi = require('./routes/index');
 
 const app = express();
 
-// Middlewares
-const corsOptions = {
-  origin: 'https://app-games-soccer.vercel.app/',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
-
-app.use(cors(corsOptions));
+//Middlewares
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
 routerApi(app);
+
 
 module.exports = app;
